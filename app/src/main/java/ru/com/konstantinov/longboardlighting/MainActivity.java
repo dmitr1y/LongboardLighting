aae19c12a9d16f831322f178d5ee4141ce1f0cd8
dmitr1y <dmitriy@konstantinov.com.ru>
2018-02-09 18:09:27 +0300

    added item to menu in toolbar added actions by taping on menu items

--- app/src/main/java/ru/com/konstantinov/longboardlighting/MainActivity.java
+++ app/src/main/java/ru/com/konstantinov/longboardlighting/MainActivity.java
@@ -1,15 +1,17 @@
 package ru.com.konstantinov.longboardlighting;
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View mode_list;
+    private View device_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        this.device_list= (View) findViewById(R.id.devices_list_view);
+        this.mode_list= (View) findViewById(R.id.modes_list_view);
     }
 
     @Override
@@ -28,16 +32,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_devices:
+                this.device_list.setVisibility(View.VISIBLE);
+                this.mode_list.setVisibility(View.GONE);
+                break;
+            case R.id.action_modes:
+                this.device_list.setVisibility(View.GONE);
+                this.mode_list.setVisibility(View.VISIBLE);
+                break;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
