465d553fbdd7b8c994247c122ec02979fc7c57a2
dmitr1y <dmitriy@konstantinov.com.ru>
2018-02-09 18:33:36 +0300

    fixed filling founded BT devices

--- app/src/main/java/ru/com/konstantinov/longboardlighting/DevicesListFragment.java
+++ app/src/main/java/ru/com/konstantinov/longboardlighting/DevicesListFragment.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 import ru.com.konstantinov.longboardlighting.Connector.DeviceFinder;
+import ru.com.konstantinov.longboardlighting.dummy.TestFinder;
 
 
 public class DevicesListFragment extends ListFragment {
@@ -20,32 +21,19 @@ public class DevicesListFragment extends ListFragment {
         // Required empty public constructor
     }
 
-    String data[] = new String[] { "Device 1", "Device 2", "Device 3", "Device 4" };
-
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-//        DeviceFinder deviceFinder = new DeviceFinder() {
-//            @Override
-//            public Set<BluetoothDevice> getBondedDevices() {
-//                return null;
-//            }
-//
-//            @Override
-//            public boolean ConnectToDevice(@NotNull BluetoothDevice device) {
-//                return false;
-//            }
-//        };
-//
-//        Set<BluetoothDevice> foundDevices=deviceFinder.getBondedDevices();
-//        List<String> deviceList = new ArrayList<String>();
-//
-//        for(BluetoothDevice bt : foundDevices)
-//            deviceList.add(bt.getName());
+        DeviceFinder deviceFinder = new TestFinder();
 
+        Set<BluetoothDevice> foundDevices = deviceFinder.getBondedDevices();
+        List<String> deviceList = new ArrayList<String>();
+
+        for (BluetoothDevice bt : foundDevices)
+            deviceList.add(bt.getName());
 
         super.onActivityCreated(savedInstanceState);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, data);
+                android.R.layout.simple_list_item_1, deviceList);
         setListAdapter(adapter);
     }
 
