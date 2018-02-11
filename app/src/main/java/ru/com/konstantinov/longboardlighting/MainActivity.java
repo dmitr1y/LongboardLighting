fa7d0d8ee26dc2b115736963d3dc8df9ba002c4e
dmitr1y <dmitriy@konstantinov.com.ru>
2018-02-11 21:03:17 +0300

    changed testFinder to normally

--- app/src/main/java/ru/com/konstantinov/longboardlighting/MainActivity.java
+++ app/src/main/java/ru/com/konstantinov/longboardlighting/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("UseSparseArrays")
     private Map<ActivityResultSubscriber, Integer> subscribers = new HashMap<>();
 
+    private DevicesListFragment devicesListFragment;
+    private ModesListFragment modesListFragment;
+
     private View mode_list;
     private View device_list;
     private TextView headerText;
@@ -49,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
         mode_list = findViewById(R.id.modes_list_view);
         headerText = findViewById(R.id.headerText);
         batteryView = findViewById(R.id.battery_view);
-
+        devicesListFragment = new DevicesListFragment();
+        modesListFragment = new ModesListFragment();
 
         headerText.setText(R.string.action_devices);
 
@@ -99,6 +103,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    public DeviceFinder getFinder(){
+        return this.deviceFinder;
     }
 
     @Override
