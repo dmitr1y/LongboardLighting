60ee4d3b12cef1d196a8e718f494c70ecb1e5772
dmitr1y <dmitriy@konstantinov.com.ru>
2018-02-12 15:47:45 +0300

    added comments

--- app/src/main/java/ru/com/konstantinov/longboardlighting/ControllerVariables.java
+++ app/src/main/java/ru/com/konstantinov/longboardlighting/ControllerVariables.java
@@ -2,13 +2,14 @@ package ru.com.konstantinov.longboardlighting;
 
 /**
  * Created by dmitriy on 12.02.18.
+ * Variables of controller enum
  */
 
 public enum ControllerVariables {
-    MODE(0),
-    BRIGHTNESS(1),
-    COLOR(2),
-    VOLTAGE(3);
+    MODE(0), // led mode
+    BRIGHTNESS(1), // led brightness
+    COLOR(2), // led color
+    VOLTAGE(3); // battery voltage
 
     private final int code;
 
